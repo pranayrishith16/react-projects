99811298f4fd6d5a22e4914aedcde37be895cf4d
pranayrishith16 <pranayrishith007@gmail.com>
2021-12-22 12:11:31 +0530

    second

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,30 +1,115 @@
+/* eslint-disable default-case */
+import {useReducer} from "react";
 import './styles.css';
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton"
+
+export const ACTIONS = {
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR:'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUATE:"evaluate",
+}
+
+function reducer(state, {type, payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      if(payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+      if(payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
+
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+    case ACTIONS.CLEAR:
+      return {}
+    
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand == null && state.previousOperand == null){
+        return state
+      }
+      if(state.previousOperand == null){
+        return{
+          ...state,
+          operation:payload.operation,
+          previousOperand:state.currentOperand,
+          currentOperand:null,  
+        }
+      }
+
+      if(state.currentOperand == null){
+        return{
+          ...state,
+          operation:payload.operation,
+        }
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation:payload.operation,
+        currentOperand:null,
+      }
+  }
+};
+
+
+function evaluate({currentOperand, previousOperand, operation}){
+  const prev = parseFloat(previousOperand);
+  const curr = parseFloat(currentOperand);
+  if(isNaN(prev) || isNaN(curr)){
+    return ""
+  }
+  let computation = "";
+  switch(operation){
+    case "+":
+      computation = prev + curr
+      break
+    case "-":
+      computation = prev - curr
+      break
+    case "*":
+      computation = prev * curr
+      break
+    case "÷":
+      computation = prev / curr
+      break
+  }
+
+  return computation.toString();
+}
 
 
 function App() {
+  const [{currentOperand, previousOperand, operation},dispatch] = useReducer(reducer,{});
   return(
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">12345</div>
-        <div className="current-operand">12345</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({type:ACTIONS.CLEAR})}>AC</button>
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <OperationButton operation="÷" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="*" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
       <button className="span-two">=</button>
     </div>
   );
